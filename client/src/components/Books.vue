0d996f576cb1dc00aecf119ddbe36b90e45faca7
ShipaShipovnik <shipovnik2020d@gmail.com>
2024-10-08 10:04:57 +0300

    All

---
+++ client/src/components/Books.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <p>books</p>
+    </div>
+</template>
+<script>
+export default {
+    name: Books,
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
---
+++ client/src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Ping from '@/components/Ping'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Ping',
+      component: Ping
+    },
+    {
+      path: '/',
+      name: 'Books',
+      component: Books
+    },
+  ],
+  mode: 'history'
+})
+
